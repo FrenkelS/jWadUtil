41bbab935c2555e0f9f6d42112c1d05f56492369
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-10-15 09:23:49 +0200

    remove deprecated methods

--- src/main/java/com/sfprod/utils/ByteBufferUtils.java
+++ src/main/java/com/sfprod/utils/ByteBufferUtils.java
@@ -8,21 +8,9 @@ public interface ByteBufferUtils {
 
 	ByteOrder DONT_CARE = ByteOrder.LITTLE_ENDIAN;
 
-	@Deprecated
-	static ByteBuffer newByteBuffer() {
-		return newByteBuffer(65536);
-	}
-
 	static ByteBuffer newByteBuffer(ByteOrder byteOrder) {
 		ByteBuffer bb = ByteBuffer.allocate(65536);
 		bb.order(byteOrder);
-		return bb;
-	}
-
-	@Deprecated
-	static ByteBuffer newByteBuffer(int capacity) {
-		ByteBuffer bb = ByteBuffer.allocate(capacity);
-		bb.order(ByteOrder.LITTLE_ENDIAN);
 		return bb;
 	}
 
